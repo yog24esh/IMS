ac556ae36a7dfaec2737d99cc24c4711376c2513
A06438_P5.Training <A06438_P5.Training@P5-H80Q882.dir.svc.accenture.com>
2019-12-05 15:55:32 +0530

    VendorServiceConsumer added author:yogesh

---
+++ src/com/accenture/ims/web/VendorServiceConsumer.java
@@ -0,0 +1,39 @@
+package com.accenture.ims.web;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.accenture.ims.business.bean.VendorBean;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class VendorServiceConsumer {
+	
+	public static final String REST_SERVICE_URL = "http://localhost:8087/project";
+
+	public static List<String> listVendorName() throws Exception {
+		RestTemplate restTemplate = new RestTemplate();
+
+		List<LinkedHashMap<String, Object>> vendorListJSON = restTemplate
+				.getForObject(REST_SERVICE_URL + "/vendor/controller/getVendors", List.class);
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		List<String> vendorListName = new ArrayList<String>();
+
+		
+		if (vendorListJSON != null) {
+			for (LinkedHashMap<String, Object> vendorObj : vendorListJSON) {
+				VendorBean vendorBean = mapper.convertValue(vendorObj, VendorBean.class);
+				vendorListName.add(vendorBean.getVendorName());
+			}
+			return vendorListName;
+		} else {
+			throw new Exception("No Vendors to Display");
+		}
+
+	}
+
+}
